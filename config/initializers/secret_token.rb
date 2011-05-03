8b2743762331619af48e01f1b22665498733435d
B V Satyaram <bvsatyaram@gmail.com>
2011-05-03 16:41:13 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickScrapperApp::Application.config.secret_token = 'd8760c90b0e87e7964877f867c43ae3010e264bf88e5a669375d8fc25118463a85b2e0f0c7fef88146b238ba46d64f86fa587f7e4950049a6980d6150a8984db'
